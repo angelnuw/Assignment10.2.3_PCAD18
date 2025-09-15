9128b13332eae64a7d511b701b6d2eff3282e393
Angel Nuw <angelnuw7@gmail.com>
2025-09-15 11:01:22 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Assignment10._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA",
+                                "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            Console.Write("input starting character for the string: ");
+            char startChar = Char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            Console.Write("Input ending character for the string: ");
+            char endChar = Char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            var result = from city in cities
+                         where city.StartsWith(startChar.ToString())
+                                && city.EndsWith(endChar.ToString())
+                         select city;
+
+            if (result.Any())
+            {
+                foreach (var city in result)
+                {
+                    Console.WriteLine($"The city starting with {startChar} and ending with {endChar} is: {city}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"No city found starting with {startChar} and ending with {endChar}");
+            }
+        }
+    }
+}
